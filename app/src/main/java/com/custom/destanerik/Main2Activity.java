282449df29ae06c6cc5d1cf3c3f7e8122f89ad82
Destan Erik <destan.erik@arneca.com>
2018-08-22 15:20:55 +0300

    Main2Activity was created to implement custom view.

--- app/src/main/java/com/custom/destanerik/Main2Activity.java
+++ app/src/main/java/com/custom/destanerik/Main2Activity.java
@@ -2,17 +2,39 @@ package com.custom.destanerik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import com.custom.destanerik.arcprogressbar.ArcProgressBar;
 import com.custom.destanerik.arcprogressbar.R;
 
-public class Main2Activity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main2Activity extends AppCompatActivity{
+
+    ArcProgressBar bar;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        ArcProgressBar bar = findViewById(R.id.customBar);
+        bar = findViewById(R.id.customBar);
+
+        ArrayList<ArcProgressModel> list = new ArrayList<>();
+
+        list.add(new ArcProgressModel("86","%","","CPU"));
+        list.add(new ArcProgressModel("45","%","","RAM"));
+        list.add(new ArcProgressModel("30","%","","NETWORK"));
+        list.add(new ArcProgressModel("15","%","","DISK"));
+
+        bar.setDisplayedValues(list);
+
+//        bar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
